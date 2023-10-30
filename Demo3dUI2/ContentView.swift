1ba08bf456b8eb22290baac44b7000c1c8a50aef
anorskdev <anorskdev@gmail.com>
2023-10-29 18:24:09 -0600

    Initial Commit

---
+++ Demo3dUI2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Demo3dUI2
+//
+//  Created by Allen Norskog on 10/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo3dUI2/Demo3dUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo3dUI2App.swift
+//  Demo3dUI2
+//
+//  Created by Allen Norskog on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo3dUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
